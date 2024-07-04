4510644a3fce714142e6a0cb76072cfa69cf1ea5
RyotaIzumi <136326820+RyotaIzumi@users.noreply.github.com>
2024-07-04 17:04:48 +0900

    HoT: initial revision

--- Heads or Tails.cpp
+++ Heads or Tails.cpp
@@ -1,13 +1,6 @@
 ﻿// Heads or Tails.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
 //
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
 // プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
 
@@ -18,3 +11,42 @@ int main()
 //   4. エラー一覧ウィンドウを使用してエラーを表示します
 //   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
 //   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
+
+#include <iostream>
+#include <string>
+#include <time.h>
+
+int countH = 0;
+int countT = 0;
+
+void playRound(int);
+std::string choose();
+
+
+int main() {
+
+    srand((unsigned int)time(NULL));
+    std::cout << "tossing a coin" << std::endl;
+    playRound(1);
+    playRound(2);
+    playRound(3);
+    std::cout << "Heads: " << countH << ", Tails: " << countT << std::endl;
+
+}
+
+void playRound(int n) {
+    std::cout << "Round " << n << ": " << choose() << std::endl;
+}
+
+std::string choose() {
+    RAND_MAX;
+    int ram = rand() % 2;
+    std::string word;
+
+    switch (ram) {
+    case 0: word = "Heads";countH++; break;
+    case 1: word = "Tails";countT++; break;
+    }
+
+    return word;
+}
